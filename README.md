27d3f5e1385dfc92c9b8dbd09b71819aec1b242a
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-08-19 01:11:44 -0500

    finished product

--- README.md
+++ README.md
@@ -2,16 +2,25 @@
 
 ## Description
 
-I was tasked with creating a Node.js app that generates an SVG file of a simple logo using inquiries that collects data from the user.
+I was tasked with creating a Node.js app that generates an SVG file of a simple logo using inquiries that collects data from the user. I used Node.js and Inquirer along with Jest for testing.
 
 ## Installation
 
-I used Node.js and Inquirer along with Jest for testing.
+In order to install this app, you would clone the [repository](https://github.com/MrMessyFace/svg-logo-maker) from GitHub. After you have cloned the repository, you would type "npm i" into the terminal to install the NPM packages. After the packages are installed, you would type "node index.js" into the terminal to start the app.
 
 ## Usage
 
-When a user opens the directory in their command line, they would install Inquirer then use Node.js on the index.js file. This would ask a series of questions that will determine the shape, text, and colors of the logo in the SVG file that will be generated.
+When a user starts the app, the terminal would ask a series of questions that will determine the shape, text, and colors of the logo in the SVG file that will be generated.
+
+![Screenshot](assets/images/svg-logo-maker.jpg)
 
 ## Credits
 
-See how it works in this [video demonstration](https://drive.google.com/file/d/1dshjHGLKM7oI8R-j7-K8-0m57R6mfw_i/view).
+Code was created using class activities from the Northwestern University Full Stack Coding Bootcamp.
+
+## Helpful Links
+
+- [Repository](https://github.com/MrMessyFace/svg-logo-maker)
+- [Video Demonstration](https://drive.google.com/file/d/1dshjHGLKM7oI8R-j7-K8-0m57R6mfw_i/view)
+
+&copy; 2023 Jessie S. Galindo. All Rights Reserved.
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 const { Circle, Triangle, Square } = require("./lib/shapes");
 
 const questions = [
   "Enter text for the logo:",
   "Enter a text color:",
   "Select a shape for the logo:",
-  "Enter a shape color:"
+  "Enter a shape color:",
 ];
 
 function writeToFile(fileName, data) {
@@ -15,57 +15,57 @@ function writeToFile(fileName, data) {
       console.log("Hmm... Didn't work.", err);
     } else {
       console.log("SVG Logo successfully generated!");
-    };
+    }
   });
-};
+}
 
 function init() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      message: questions[0],
-      name: 'text',
-      default: '(Must not be more than 3 characters)'
-    },
-    {
-      type: 'input',
-      message: questions[1],
-      name: 'textColor'
-    },
-    {
-      type: 'list',
-      message: questions[2],
-      name: 'shape',
-      choices: [
-        'circle',
-        'square',
-        'triangle'
-      ]
-    },
-    {
-      type: 'input',
-      message: questions[3],
-      name: 'shapeColor'
-    },
-  ])
-    .then(answers => {
-      // console.log(answers);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "text",
+        default: "(Must not be more than 3 characters)",
+      },
+      {
+        type: "input",
+        message: questions[1],
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: questions[2],
+        name: "shape",
+        choices: ["circle", "square", "triangle"],
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "shapeColor",
+      },
+    ])
+    .then((answers) => {
       let shape;
 
       if (answers.shape == "circle") {
-        shape = new Circle(answers.text, answers.textColor, answers.shapeColor)
-      };
+        shape = new Circle(answers.text, answers.textColor, answers.shapeColor);
+      }
 
       if (answers.shape == "square") {
-        shape = new Square(answers.text, answers.textColor, answers.shapeColor)
-      };
+        shape = new Square(answers.text, answers.textColor, answers.shapeColor);
+      }
 
       if (answers.shape == "triangle") {
-        shape = new Triangle(answers.text, answers.textColor, answers.shapeColor)
-      };
+        shape = new Triangle(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+      }
 
       writeToFile("logo.svg", shape.render());
     });
-};
+}
 
 init();
